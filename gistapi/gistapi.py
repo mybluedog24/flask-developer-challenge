96eb9dc2e02e29e82e3caa2fc4dfaa227b2394cd
mybluedog24 <frankchen4096@gmail.com>
2016-07-25 13:21:41 -0400

    Basic requirement done.

--- gistapi/gistapi.py
+++ gistapi/gistapi.py
@@ -11,6 +11,7 @@ providing a search across all public Gists for a given Github account.
 
 import requests
 from flask import Flask, jsonify, request
+import re
 
 
 # *The* app object
@@ -40,8 +41,8 @@ def gists_for_user(username):
     gists_url = 'https://api.github.com/users/{username}/gists'.format(
             username=username)
     response = requests.get(gists_url)
-    # BONUS: What failures could happen?
-    # BONUS: Paging? How does this work for users with tons of gists?
+    # TODO: BONUS: What failures could happen?
+    # TODO: BONUS: Paging? How does this work for users with tons of gists?
 
     return response.json()
 
@@ -60,6 +61,16 @@ def search():
     """
     post_data = request.get_json()
     # BONUS: Validate the arguments?
+    # Validate keys
+    if set(["username", "pattern"]) != set(post_data):
+        raise TypeError("request must have excatly keys ['username', 'pattern']: ", post_data.keys())
+    # Validate data type of username
+    if not isinstance(post_data["username"], unicode):
+        raise TypeError("username must be unicode type: ", type(post_data["username"]))
+    # Validate data type of pattern
+    if not isinstance(post_data["pattern"], unicode):
+        raise TypeError("pattern must be unicode type: ", type(post_data["pattern"]))
+    # BONUS DONE
 
     username = post_data['username']
     pattern = post_data['pattern']
@@ -67,20 +78,73 @@ def search():
     result = {}
     gists = gists_for_user(username)
     # BONUS: Handle invalid users?
+    if "message" in gists:
+        raise ValueError("username %s: %s" % (str(username), str(gists["message"])))
+    # BONUS DONE
+
+    result['matches'] = []
+    result['status'] = 'success'
 
     for gist in gists:
-        # REQUIRED: Fetch each gist and check for the pattern
-        # BONUS: What about huge gists?
-        # BONUS: Can we cache results in a datastore/db?
-        pass
+        gist_url = gist["url"]
+        html_url = gist["html_url"]
+        response = requests.get(gist_url)
+        status_code = response.status_code
 
-    result['status'] = 'success'
+        # 1. Check status code
+        if status_code != 200:
+            result['status'] = 'failure'
+            continue
+
+        # 2. Search pattern without checking truncation
+        match = re.search(pattern, response.content)
+        if match:
+            result_url = html_url.replace(gist["id"], username + "/" + gist["id"])
+            result['matches'].append(result_url)
+            continue
+
+        # 3. If no match is found, search truncated files
+        #    Documentation about truncation: https://developer.github.com/v3/gists/#truncation
+        # TODO: Handle Truncation case1: total number exceeds 300 files
+        # TODO: Find a better way to handle this case instead of clone the gist
+        # My current idea is clone the gist and store it to a folder.
+        # And then search the entire folder to find the pattern.
+        # After that, delete the temp folder. But this requires more space and
+        # low performance. Also I don't like executing external commands..
+
+        # if gist["truncated"]:
+        #     git_pull_url = gist["git_pull_url"]
+        #     temp_folder = "./temp_folder/"
+        #     os.system("git clone %s %s" % (git_pull_url, temp_folder))
+
+        # 4. If no match is found, search truncated files (including files from 3.)
+        #    Documentation about truncation: https://developer.github.com/v3/gists/#truncation
+        #    Handle Truncation case2: file content exceeds limit (up to one megabyte)
+        # TODO: Find a better way to handle this case Instead of download the whole content
+        # If the file is very large, it will slow the process.
+
+        gist_url_response = requests.get(gist_url)
+        gist_json = gist_url_response.json()
+        files = gist_json["files"]
+        for key in files:
+            if files[key]["truncated"]:
+                file_response = requests.get(files[key]["raw_url"])
+                match = re.search(pattern, file_response.content)
+                if match:
+                    result_url = html_url.replace(gist["id"], username + "/" + gist["id"])
+                    result['matches'].append(result_url)
+
+    # REQUIRED: Fetch each gist and check for the pattern
+    # REQUIRED DONE
+    # TODO: BONUS: What about huge gists?
+    # TODO: BONUS: Can we cache results in a datastore/db?
+
     result['username'] = username
     result['pattern'] = pattern
-    result['matches'] = []
 
     return jsonify(result)
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
+
--- tests/test_gistapi.py
+++ tests/test_gistapi.py
@@ -5,23 +5,36 @@ import tempfile
 import pytest
 
 import gistapi
+import requests
 
 
 @pytest.fixture
 def client(request):
-    #db_fd, gistapi.app.config['DATABASE'] = tempfile.mkstemp()
+    # db_fd, gistapi.app.config['DATABASE'] = tempfile.mkstemp()
     gistapi.app.config['TESTING'] = True
     client = gistapi.app.test_client()
 
-    #with gistapi.app.app_context():
+    # with gistapi.app.app_context():
     #    gistapi.init_db()
-    
-    #def teardown():
+
+    # def teardown():
     #    os.close(db_fd)
     #    os.unlink(flaskr.app.config['DATABASE'])
-    #request.addfinalizer(teardown)
+    # request.addfinalizer(teardown)
 
     return client
+
+
+######################################################
+# IMPORTANT: "For unauthenticated requests, the rate limit allows you to make up to 60 requests per hour."
+# check limit: curl -i https://api.github.com/users/whatever
+# more details: https://developer.github.com/v3/#rate-limiting
+######################################################
+
+# Skip tests if status=403 Forbidden. It may caused by rate limit exceeded.
+forbidden = pytest.mark.skipif(
+    requests.get("https://api.github.com/users/whatever").status_code == 403,
+    reason="403 Forbidden")
 
 
 def test_ping(client):
@@ -30,16 +43,99 @@ def test_ping(client):
     assert b'pong' in rv.data
 
 
+@forbidden
 def test_search(client):
     """Start with a passing test."""
     post_data = {'username': 'justdionysus', 'pattern': 'TerbiumLabsChallenge_[0-9]+'}
-    rv = client.post('/api/v1/search', 
+    rv = client.post('/api/v1/search',
                      data=json.dumps(post_data),
-                     headers={'content-type':'application/json'})
+                     headers={'content-type': 'application/json'})
     result_dict = json.loads(rv.data.decode('utf-8'))
-    expected_dict = {'status': 'success', 
+    expected_dict = {'status': 'success',
                      'username': 'justdionysus',
                      'pattern': 'TerbiumLabsChallenge_[0-9]+',
                      'matches': ['https://gist.github.com/justdionysus/6b2972aa971dd605f524']}
+
     assert result_dict == expected_dict
+
+
+@forbidden
+def test_invalid_arguments_keys(client):
+    """Start with a passing test."""
+    post_data = {'username2': 'justdionysus', 'pattern2': 'TerbiumLabsChallenge_[0-9]+'}
+    with pytest.raises(TypeError):
+        rv = client.post('/api/v1/search',
+                         data=json.dumps(post_data),
+                         headers={'content-type': 'application/json'})
+        json.loads(rv.data.decode('utf-8'))
+
+
+@forbidden
+def test_invalid_arguments_username_value(client):
+    """Start with a passing test."""
+    post_data = {'username': 2, 'pattern': 'TerbiumLabsChallenge_[0-9]+'}
+    with pytest.raises(TypeError):
+        rv = client.post('/api/v1/search',
+                         data=json.dumps(post_data),
+                         headers={'content-type': 'application/json'})
+        json.loads(rv.data.decode('utf-8'))
+
+
+@forbidden
+def test_invalid_arguments_pattern_value(client):
+    """Start with a passing test."""
+    post_data = {'username': "justdionysus", 'pattern': 2}
+    with pytest.raises(TypeError):
+        rv = client.post('/api/v1/search',
+                         data=json.dumps(post_data),
+                         headers={'content-type': 'application/json'})
+        json.loads(rv.data.decode('utf-8'))
+
+
+@forbidden
+def test_search_nonexistent_username(client):
+    """Start with a passing test."""
+    post_data = {'username': '++--', 'pattern': 'TerbiumLabsChallenge_[0-9]+'}
+    with pytest.raises(ValueError) as errorinfo:
+        rv = client.post('/api/v1/search',
+                         data=json.dumps(post_data),
+                         headers={'content-type': 'application/json'})
+        json.loads(rv.data.decode('utf-8'))
+
+    assert errorinfo.value.message == "username ++--: Not Found"
+
+####################################
+# These two tests are for truncation case1 and case2.
+####################################
+# def test_truncation_case1_exceeds_300_files(client):
+#     """There are 310 files in one gist. Each file has one integer between 1-310.
+#     By checking the list from requests.get(), number 90-99 are truncated."""
+#     post_data = {'username': 'mybluedog24', 'pattern': '\\b(9\\d)\\b'}
+#     rv = client.post('/api/v1/search',
+#                      data=json.dumps(post_data),
+#                      headers={'content-type': 'application/json'})
+#     result_dict = json.loads(rv.data.decode('utf-8'))
+#     expected_dict = {'status': 'success',
+#                      'username': 'mybluedog24',
+#                      'pattern': '\\b(9\\d)\\b',
+#                      'matches': ['https://gist.github.com/mybluedog24/9b79db5c345556bf8e9cb6359ec8a0ba']}
+#
+#     assert result_dict == expected_dict
+#
+#
+# def test_truncation_case2_file_content_exceeds_limit(client):
+#     """There are 310 files in one gist. Each file has one integer between 1-310.
+#     By checking the list from requests.get(), number 90-99 are truncated."""
+#     post_data = {'username': 'mybluedog24', 'pattern': 'LASTLINE'}
+#     rv = client.post('/api/v1/search',
+#                      data=json.dumps(post_data),
+#                      headers={'content-type': 'application/json'})
+#     result_dict = json.loads(rv.data.decode('utf-8'))
+#     expected_dict = {'status': 'success',
+#                      'username': 'mybluedog24',
+#                      'pattern': 'LASTLINE',
+#                      'matches': ['https://gist.github.com/mybluedog24/d3e49c8e4a8081a2e40125f48de70317']}
+#
+#     assert result_dict == expected_dict
+
 
